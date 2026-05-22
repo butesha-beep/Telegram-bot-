bfde980cb260baba83b05fae9a6e392199a12138
butesha-beep <butesha@icloud.com>
2026-05-22 12:45:27 +0200

    test vars

--- bot.py
+++ bot.py
@@ -21,6 +21,8 @@ if not TOKEN:
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 DATABASE_URL = os.getenv("DATABASE_URL") or os.getenv("DATABASE_PUBLIC_URL")
+print("TEST_VAR =", os.getenv("TEST_VAR"))
+print("DATABASE_URL exists =", bool(os.getenv("DATABASE_URL")))
 
 if not DATABASE_URL:
     available_vars = [key for key in os.environ.keys() if "DATABASE" in key or key.startswith("PG")]
